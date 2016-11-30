dda92181c31d2da56fc143d14aa1a2e650dfff5a
WINDOWS-IEQD4KU\Aviha <Avihaihai737@gmail.com>
2016-11-30 08:14:37 +0200

    Added person class that holds all the features of of a person.

---
+++ GuaranteedRateHW/ClassCode/People.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.Remoting.Messaging;
+using System.Threading.Tasks;
+
+namespace GuaranteedRateHW.ClassCode
+{
+    public class People
+    {
+        public List<Person> ListOfPeople;
+
+        public People()
+        {
+            ListOfPeople = new List<Person>();
+        }
+
+        public Boolean ReadFile(string filePath, out string errorMessage)
+        {
+            errorMessage = "";
+            try
+            {
+                //# Check if file is exists
+                if (File.Exists(filePath))
+                {
+                    
+                    //# Open file.txt with the Using statement.
+                    using (var sr = new StreamReader(filePath))
+                    {
+                        string line = sr.ReadLine();
+                        //# Loop over each line in file, While line is Not Nothing.
+                        while (((line != null)))
+                        {
+                            ListOfPeople.Add(new Person(line));
+                            //# Read in the next line.
+                            line = sr.ReadLine();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot read file: " + filePath);
+                Console.WriteLine(ex.Message);
+            }
+            return true;
+        }
+
+        public IEnumerable<Person> SortByGenderLastName()
+        {
+            return from person in ListOfPeople
+                   orderby person.Gender ascending, person.LastName ascending
+                   select person;
+        }
+
+        public IEnumerable<Person> SortByBirthDate()
+        {
+            return from person in ListOfPeople
+                orderby person.DateOfBirth ascending
+                select person;
+        }
+
+        public IEnumerable<Person> SortByLastName()
+        {
+            return from person in ListOfPeople
+                orderby person.LastName descending
+                select person;
+        }
+
+
+    }
+}
---
+++ GuaranteedRateHW/ClassCode/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace GuaranteedRateHW.ClassCode
+{
+  
+    public class Person
+    {
+    
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public  string Gender  { get; set; }
+        public string FavoriteColor  { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        [JsonConstructor]
+        public Person(string FirstName, string LastName, string Gender, string FavoriteColor, DateTime DateOfBirth)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Gender = Gender;
+            this.FavoriteColor = FavoriteColor;
+            this.DateOfBirth = DateOfBirth;
+        }
+        public Person(string inputLine)
+        {
+            string[] tokens = inputLine.Split(new char[] { '|', ',', ' ' });
+            if (tokens.Length != 5)
+            {
+                Console.WriteLine(string.Format("The number of tokens {0} needs to be 5. Input Line: \"{1}\"", tokens.Length.ToString(), inputLine));
+                throw new Exception(string.Format("The number of tokens {0} needs to be 5. Input Line: \"{1}\"", tokens.Length.ToString(), inputLine));
+            }
+            DateTime dateOfBirth;
+            if (!DateTime.TryParse(tokens[4], out dateOfBirth))
+            {
+                Console.WriteLine(string.Format("Invalid Date of Birth Entered: {0}. Input Line: \"{1}\"", tokens[4], inputLine));
+                throw new Exception(string.Format("Invalid Date of Birth Entered: {0}.Input Line: \"{1}\"", tokens[4], inputLine));
+            }
+            FirstName = tokens[0];
+            LastName = tokens[1];
+            Gender = tokens[2];
+            FavoriteColor = tokens[3];
+             DateOfBirth= dateOfBirth;
+        }
+    }
+}
